dd4d41b3daa901a70479b1eaaa86d4dd52e72a59
Yisheng Jiang2 <yishengjiang13@Yishengs-MBP.socal.rr.com>
2015-09-18 16:23:22 -0700

    day2

--- functions.js
+++ functions.js
@@ -1,7 +1,19 @@
 var f={};
-f.google=function(query,cb){
+
+f.google=function(query,cb,prog){
 	query = encodeURIComponent(query);
+	if(prog && typeof prog === "function") prog();
 	$.getJSON("https://ajax.googleapis.com/ajax/services/search/web?v=1.0&q="+query+"&callback=?",function(ret){
-		cb(ret);
+		if(ret.responseData.results){
+			var data = [];
+			$(ret.responseData.results).each(function(i,e){
+				data.push(e.url);
+			});
+			cb(data);
+		}
 	});
+}
+
+f.weather=function(zipcode,cb){
+
 }
